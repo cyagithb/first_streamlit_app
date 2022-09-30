bdde59d327c9a4e8187e5efdcdede6625c8f07c1
Cathy Alcause <114638039+cyagithb@users.noreply.github.com>
2022-09-30 14:34:10 +0800

    Update streamlit_app.py
    added fruit choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -23,6 +23,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
+fruit.choice=streamlit.text_input('What fruit would you like information about?'. 'Kiwi')
+streamlit.write('The user entered', fruut_choice)
+
 #Comment out raw json
 #streamlit.text(fruityvice_response.json())
 
